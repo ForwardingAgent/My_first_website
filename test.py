735ae96ff3a5bd23314ea42b30a407ac8c285fed
ForwardingAgent <nikolaspdk@gmail.com>
2023-04-13 18:20:35 +0300

    test

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-n = input()
-n = map(int, n)
-print(n)
+#class Point:
+#    def __init__(self, x, y):
+#        self.x = x
+#        self.y = y
+#    
+#    def clone(self):
+#        pass
+#
+#
+#x = 3
+#y = 4
+#pt = Point(x, y)
+#pt_clone = clone()
+
+
+
+T = input()
+R = input()
+s = T + R
+if s == 'ножницыбумага' or 'каменьножницы' or 'бумагакамень':
+    print('Тимур')
+elif R == T:
+    print('ничья')
+else:
+    print('Руслан')
