15e40feb4fa22bf5ec2189adab94d2f1b91c5c29
ForwardingAgent <nikolaspdk@gmail.com>
2023-04-16 20:33:52 +0300

    del test

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def clone(self):
-        pass
-
-
-x = 3
-y = 4
-pt = Point(x, y)
-pt_clone = Point(clone())
-
 
 T = input()
 R = input()
