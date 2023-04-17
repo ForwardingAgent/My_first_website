c783515cce5b2df5f36c9b38f642bfbb90513124
ForwardingAgent <nikolaspdk@gmail.com>
2023-04-17 19:55:20 +0300

    add test

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 
-T = input()
-R = input()
-s = T + R
-if s == 'ножницыбумага' or 'каменьножницы' or 'бумагакамень':
-    print('Тимур')
-elif R == T:
-    print('ничья')
-else:
-    print('Руслан')
+# T = input()
+# R = input()
+# s = T + R
+# if s == 'ножницыбумага' or 'каменьножницы' or 'бумагакамень':
+#     print('Тимур')
+# elif R == T:
+#     print('ничья')
+# else:
+#     print('Руслан')
+
+
+list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+maximum = map(max, list1)
+print(maximum)
