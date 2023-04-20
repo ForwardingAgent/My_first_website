99bf3dc5e455e2a48229c05a5eaeef96726c3a8b
ForwardingAgent <nikolaspdk@gmail.com>
2023-04-20 15:05:40 +0300

    OOP Balakirev

--- test.py
+++ test.py
@@ -1,47 +1,33 @@
 
-# T = input()
-# R = input()
-# s = T + R
-# if s == 'ножницыбумага' or 'каменьножницы' or 'бумагакамень':
-#     print('Тимур')
-# elif R == T:
-#     print('ничья')
-# else:
-#     print('Руслан')
+class Book:
+    def __init__(self, author, title, price):
+        self.__author = author
+        self.__title = title
+        self.__price = price
+
+    def set_title(self, title):  # запись в локальное приватное свойство __title объектов класса Book значения title;
+        self.__title = title
+
+    def set_author(self, author):  # запись в локальное приватное свойство __author объектов класса Book значения author;
+        self.__author = author
+
+    def set_price(self, price):  # запись в локальное приватное свойство __price объектов класса Book значения price;
+        self.__price = price
+
+    def get_title(self):  # получение значения локального приватного свойства __title объектов класса Book;
+        return self.__title
+
+    def get_author(self):  # получение значения локального приватного свойства __author объектов класса Book;
+        return self.__author
+
+    def get_price(self):  # получение значения локального приватного свойства __price объектов класса Book;
+        return self.__price
 
 
-class Video:
-    def __init__(self):
-        pass
-
-    def create(self, name):  # для задания имени name текущего видео (метод сохраняет имя name в локальном атрибуте name объекта класса Video);
-        self.name = name
-
-    def play(self):  # для воспроизведения видео (метод выводит на экран строку "воспроизведение видео <name>").
-        return print('воспроизведение видео', self.name)
-
-
-class YouTube:
-    videos = []  # для хранения добавленных объектов класса Video (изначально список пуст).
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def add_video(cls, video):  # для добавления нового видео (метод помещает объект video класса Video в список);
-        cls.videos.append(video)
-        print(cls.videos)
-
-    @classmethod
-    def play(cls, video_indx):  # для проигрывания видео из списка по указанному индексу (индексация с нуля) (здесь cls - ссылка на класс YouTube). И список (тоже внутри класса YouTube):
-        # Video.play(cls.videos[video_indx])  моё, вызываю def play из Video передавая параметр, а в Video нет play с параметром
-        cls.videos[video_indx].play()
-
-v1, v2 = Video(), Video()
-v1.create('Python')
-v2.create('Python ООП')
-YouTube.add_video(v1)
-YouTube.add_video(v2)
-YouTube.play(0)
-YouTube.play(1)
-
+book = Book('50 Cent', 'Aboba', 999)
+book.set_title('Amogus')
+book.set_author('NLE Choppa')
+book.set_price(123)
+book.get_title()
+book.get_author()
+book.get_price()
